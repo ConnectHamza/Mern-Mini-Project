741a990abf5946b5430a0297e77581d57a1f2178
Hamza Ahmed <smmwp220@gmail.com>
2026-01-06 00:30:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const app = express();
+const userModel = require('./models/user');
+const postModel = require('./models/post');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.get('/login', (req, res) => {
+    res.render("Login");
+});
+
+app.get('/profile', isLoggedIn, (req, res) => {
+    console.log(req.user);
+    res.render("login");
+});
+
+app.post('/register', async (req, res) => {
+    let {email, password, username, name, age} = req.body;
+
+    let user = await userModel.findOne({email});
+    if(user) return res.status(500).send('User Already Registered');
+
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => { 
+            await userModel.create({
+                username,
+                email,
+                age,
+                name, 
+                password: hash
+            });
+
+           let token =  jwt.sign({email: email, userid: user._id}, "hello");
+           res.cookie ('token', token);
+           res.send('Registered');
+        })
+    })
+});
+
+app.post('/login', async (req, res) => {
+    let {email, password, username, name, age} = req.body;
+
+    let user = await userModel.findOne({email});
+    if(!user) return res.status(500).send('Something Went Wrong');
+
+    bcrypt.compare(password, user.password, function (err, result){
+        if(result) {
+            let token =  jwt.sign({email: email, userid: user._id}, "hello");
+            res.cookie ('token', token);
+            res.status(200).send('You can Login');
+        }
+        else res.redirect('/login');
+    })
+});
+
+app.get('/logout', (req, res) => {
+    res.cookie('token', '');
+    res.redirect("/login");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.cookies.token === "") {
+        res.send("You must be logged in");
+    } else {
+        try {
+            let data = jwt.verify(req.cookies.token, "hello");
+            req.user = data;
+            next();
+        } catch(err) {
+            res.send("Invalid token. Please login again.");
+        }
+    }
+}
+
+app.listen(3000);
